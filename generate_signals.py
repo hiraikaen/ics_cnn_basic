4ca2810b51d4f93e6a05b32d4a9e813984b261af
myuser <hiraikaen@gmail.com>
2018-12-27 19:30:27 +0900

    normal, abnormal_attack, abnormal_offset function added

--- generate_signals.py
+++ generate_signals.py
@@ -1,13 +1,17 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-
 from scipy import signal
 import numpy as np
+import time
+import csv
 
-def bit_signal(t):#, f):
-        #return signal.square(2*np.pi*f*t, duty=0.5).astype('float64')
-        return signal.square(t).astype('float64')
+coef = 6
+width = 200*coef
+
+def bit_signal(t):
+    #return signal.square(2*np.pi*f*t, duty=0.5).astype('float64')
+    return signal.square(t).astype('float64')
     
 np.random.seed(2)
 
@@ -32,20 +36,35 @@ N = 10
 #label,1,2,3,4,5,6,7,8,9,10
 #0,154,232,....
 
-plt.figure()
+#plt.figure()
 
-def bit_signal_noise(coef_f, cycle=5, rate=100, level_noise=1.5):
+# def bit_signal_noise(coef_f, cycle=5, rate=100, level_noise=1.5):
+#     t = np.linspace(0, 2*np.pi*cycle, 2*np.pi*cycle*rate, endpoint=False)
+#     noise = np.random.uniform(-pow(0.1,level_noise), pow(0.1, level_noise), [1,int(2*np.pi*cycle*rate)])[0]
+#     data = signal.square(t/pow(2,coef_f)) + noise
+#     data = data + abs(min(data))
+#     data = data/max(data)*255.0
+#     data = data.astype(int)
+    
+#     return t, data
+
+def bit_signal_noise(coef_f, cycle=5, rate=100, level_noise=1.5, offset=0):
     t = np.linspace(0, 2*np.pi*cycle, 2*np.pi*cycle*rate, endpoint=False)
+    t = t+offset
     noise = np.random.uniform(-pow(0.1,level_noise), pow(0.1, level_noise), [1,int(2*np.pi*cycle*rate)])[0]
     data = signal.square(t/pow(2,coef_f)) + noise
+    data = data + abs(min(data))
+    data = data/max(data)*255.0
+    data = data.astype(int)
+    
     return t, data
 
 level_noise = 1.5
-rate = 50 #resolution per cycle
-max_coef_f = 5
-cycle = 100
+rate = 20 #resolution per cycle
+max_coef_f = 4
+cycle = 50
 
-data_box = 'label'
+data_box = ''
 for i in range(max_coef_f):
     for j in range(int(rate)):
         buf_str = ','+str(i)+'x'+str(j)
@@ -53,38 +72,157 @@ for i in range(max_coef_f):
     buf_str = '\n'
     data_box += buf_str
 
+
+def compose_signals(max_coef_f, attack=False, attack_rate=0.2, visualize=False, offset_idx=None, offset_vol=None):
+    plt.figure()
+    composed_data = list()
+    for i in range(max_coef_f):
+        coef_f = i
+
+        # if i == offset_idx:
+        #     offset_vol = offset_vol
+        #     print('   >>> adding offset:', offset_vol, 'to_idx:', offset_idx)
+        # else:
+        #     offset_vol = 0
+
+        if i == offset_idx:
+            t, data = bit_signal_noise(coef_f=coef_f, cycle=cycle, rate=rate, offset=offset_vol)
+            print('   >>> adding offset:', offset_vol, 'to_idx:', offset_idx)
+        else:
+            t, data = bit_signal_noise(coef_f=coef_f, cycle=cycle, rate=rate)
+            
+        if attack:
+            #add max value in signal in random spots
+            rand_spots = np.random.randint(0, len(data), size=int(len(data)*attack_rate))
+            bar = 10
+            for idx in rand_spots:
+                if idx+bar < len(data):
+                    data[idx:idx+bar] = 0
+            
+            print('attack', data[:10], type(data), max(data), min(data))
+
+        elif offset_idx != None:
+            print('attack offset', data[:10], type(data), max(data), min(data))
+
+        else:
+            print('normal', data[:10], type(data), max(data), min(data))
+            
+            
+        if visualize:
+            plt.subplot(max_coef_f,1,coef_f+1)
+            plt.plot(t, data)
+        print('length of each slot:', len(data))
+        composed_data.append(data)
+        
+    #print('total length t:', len(composed_data[0]))
+    #input('ok?')
+    if visualize:
+        if attack:
+            print('save attack...')
+            plt.savefig('abnormal_composed.pdf')
+        elif offset_idx != None:
+            print('save attack_offset...')
+            plt.savefig('abnormal_offset_composed.pdf')            
+        else:
+            print('save normal...')
+            plt.savefig('normal_composed.pdf')
+    print('len of composed_data:', len(composed_data)*len(composed_data[0]))
+    return composed_data
+    
 #slice and insert data
-buf_data = list()
-for i in range(max_coef_f):
-    coef_f = i
-    t, data = bit_signal_noise(coef_f=coef_f, cycle=cycle, rate=rate)
-    plt.subplot(max_coef_f,1,coef_f+1)
-    plt.plot(t, data)
-    buf_data.append(data)
-   
-print(buf_data)
-input('ok?')
+# normal_data = list()
+# for i in range(max_coef_f):
+#     coef_f = i
+#     t, data = bit_signal_noise(coef_f=coef_f, cycle=cycle, rate=rate)
+#     plt.subplot(max_coef_f,1,coef_f+1)
+#     plt.plot(t, data)
+#     normal_data.append(data)
 
-width = rate
-# print('dbg:', buf_data[0][0].itemsize-width)
-#print(buf_data[0].size)
+#compose normal signal
+normal_data = compose_signals(max_coef_f, attack=False, visualize=True)
+#compose abnormal signal (by zero bar noise)
+abnormal_data = compose_signals(max_coef_f, attack=True, attack_rate=0.005, visualize=True)
 
-#
+np.random.seed(3)
+normal_test_data = compose_signals(max_coef_f, attack=False, visualize=True)
+#compose abnormal signal (by zero bar noise)
+abnormal_test_data = compose_signals(max_coef_f, attack=True, attack_rate=0.005, visualize=True)
+
+np.random.seed(4)
+abnormal_offset_train_data = compose_signals(max_coef_f, attack=False, attack_rate=0.0, visualize=True, offset_idx=1, offset_vol=20)
+
+np.random.seed(5)
+abnormal_offset_test_data = compose_signals(max_coef_f, attack=False, attack_rate=0.0, visualize=True, offset_idx=1, offset_vol=20)
+
+
+#todo
+#compose abnormal signal (by latency offset)
+#abnormal_data_latency = ...
+
+#print('total length t:', len(normal_data[0]))
+#input('ok?')
+    
+plt.savefig('tmp.pdf')
+    
+#print(normal_data)
+#input('ok?')
+
 #loop have to be rebuilt
 #
-for i in range(buf_data[0].size-width):
-    #data_box = '0,'+','.join(str(buf_data[i]))+'\n'
-    data_box += '0,'
-    for j in range(buf_data[i] - width):
-        idx = 0
-        for number in buf_data[i][idx:width]:
-            buf_str = ','+str(round(number,4))
-            data_box += buf_str
-    buf_str += '\n'
-    print(data_box)
-    print('size:', len(data_box), len(data_box[0]))
-    input('ok?')
-torch.save(data_box, open('traindata.pt', 'wb'))
-    
+#for at t=0,1,2,... size-width
+#   add label to buf
+#   for at s=0,1,2,...n(sensors)
+#      for at t_cursor=0,1,...,width
+#         add data[s, t_cursor] to buf
+#   add n to data
+#   add buf to data
 
-plt.savefig('tmp4.pdf')
+def make_images(ts_data, width, label):
+    time_start = time.time()
+    images = list()
+    for t in range(len(ts_data[0])-width):
+        buf = [label]
+        #for s in range(len(ts_data)):
+        #print('len(ts_data) == 4?', len(ts_data), 't:', t, t+width)
+        #input('ok?')
+        for s in range(len(ts_data)):
+            #buf += list(ts_data[s][t:t+width])
+            buf += ts_data[s][t:t+width].tolist() #faster
+        # print('a image:', buf)
+        # input('ok?')
+        images.append(buf)
+        #print('t:', t, 'n of images:', len(images), 'n of each image:', len(buf))
+        if t % 3000 == 0:
+            print('progress:', t, '/', len(ts_data[0])-width)
+            
+    time_elapsed = time.time() - time_start
+    print('total num of image:', len(images))
+    print('time elapsed:', time_elapsed)
+    
+    return images
+
+#make normal_data to images
+normal_images = make_images(normal_data, width, 0)
+#make abnormal_data to images
+abnormal_images = make_images(abnormal_data, width, 1)
+abnormal_offset_train_images =  make_images(abnormal_offset_train_data, width, 2)
+
+#make normal_data to images test
+normal_test_images = make_images(normal_test_data, width, 0)
+#make abnormal_data to images test
+abnormal_test_images = make_images(abnormal_test_data, width, 1)
+abnormal_offset_test_images =  make_images(abnormal_offset_test_data, width, 2)
+
+print('writing csv files...')
+with open("ics_train.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(normal_images)
+    writer.writerows(abnormal_images)
+    writer.writerows(abnormal_offset_train_images)
+
+#test data == train data same temporarily
+with open("ics_test.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(normal_test_images)
+    writer.writerows(abnormal_test_images)
+    writer.writerows(abnormal_offset_test_images)
---
+++ ics_cnn.py
@@ -0,0 +1,359 @@
+# from matplotlib import pyplot as plt
+# import pandas as pd
+# import numpy as np
+# import torch.nn as nn
+# import torch.optim as optim
+# import torchvision
+# from torch.autograd import Variable
+# import torch
+# from torchvision import transforms
+
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torchvision.utils import make_grid
+
+import math
+import random
+
+from PIL import Image, ImageOps, ImageEnhance
+import numbers
+
+import matplotlib.pyplot as plt
+
+if 0:
+    filename_train = './mnist_train.csv'
+    filename_test = './mnist_test.csv'
+    COEF = 1
+    SIZE_X = 28 #200
+    SIZE_Y = 28 #4
+    batch_size = 64
+    n_classes = 10
+
+else:
+    filename_train = './ics_train.csv'
+    filename_test = './ics_test.csv'
+    COEF = 6
+    SIZE_X = int(200*COEF)
+    SIZE_Y = 4
+    batch_size = 16
+    n_classes = 3#2
+
+    
+train = pd.read_csv(filename_train)
+test = pd.read_csv(filename_test)
+print(train.shape)
+print(test.shape)
+
+#todo:label
+trail_label = train.iloc[:,:1]
+test_label = test.iloc[:,:1]
+# train = train.iloc[:,1:]
+# test = test.iloc[:,1:]
+
+n_class = len(set(train.iloc[:, :1]))
+n_train = len(train)
+n_test = len(test)
+n_pixels = len(train.columns)
+
+#print(n_train, n_test, n_pixels)
+
+from torchvision.utils import make_grid
+
+#show number randomly
+def pick(train,start, end, size, filename):
+    random_sel = np.random.randint(start, end, size=size)
+    #print('random_sel:', random_sel)
+    grid = make_grid(torch.Tensor((train.iloc[random_sel, 1:].values/255.).reshape((-1, SIZE_Y, SIZE_X))).unsqueeze(1))#, nrow=size)
+    #print(grid)
+    #input('ok?')
+    #grid = make_grid(torch.Tensor((train.iloc[random_sel, 1:].values/255.).reshape((-1, SIZE_X, SIZE_Y))), nrow=size)
+    plt.rcParams['figure.figsize'] = (24, 24)#x,y
+    
+    #plt.imshow(grid.numpy().transpose())
+    
+    #print('dbg1', grid.numpy().transpose())
+    plt.axis('off')
+    print(*list(train.iloc[random_sel, 0].values), sep = ', ')
+    #plt.savefig(filename) 
+    #plt.show()
+
+def pick2(train, start, end, size, filename):
+    random_sel = np.random.randint(start, end, size=size)
+    #print('len of each image:', len(train.iloc[0, 1:].values))#4800
+    data = torch.Tensor((train.iloc[0, 1:].values/255.).reshape((-1, SIZE_Y, SIZE_X)))
+    # print(data.numpy())
+    # print(len(data.numpy()))
+    # print(len(data.numpy()[0]))
+    # print(len(data.numpy()[0][0]))
+    plt.imshow(data.numpy()[0], cmap='gray')
+    plt.axis('off')
+    #plt.show()
+
+n_pick = 10
+pick(train, 0, 1000, n_pick, "normal_100.pdf")
+pick(train, n_train-1-1000, n_train-1, n_pick, "abnormal_100.pdf")
+#exit(1)
+
+#show label histogram
+#plt.figure()
+#plt.hist(train.iloc[:,0])
+#plt.show()
+
+
+from torch.utils.data import DataLoader, Dataset
+        
+class MNIST_data(Dataset):
+    """MNIST dtaa set"""
+    
+    def __init__(self, file_path, 
+                 transform = transforms.Compose([transforms.ToPILImage(), transforms.ToTensor(), 
+                     transforms.Normalize(mean=(0.5,), std=(0.5,))])
+                ):
+        
+        df = pd.read_csv(file_path)
+        
+        if len(df.columns) == n_pixels:
+            # test data
+            self.X = df.iloc[:,1:].values.reshape((-1,SIZE_Y,SIZE_X)).astype(np.uint8)[:,:,:,None]
+            self.y = torch.from_numpy(df.iloc[:,0].values)
+            #self.y = None
+        else:
+            # training data
+            self.X = df.iloc[:,1:].values.reshape((-1,SIZE_Y,SIZE_X)).astype(np.uint8)[:,:,:,None]
+            self.y = torch.from_numpy(df.iloc[:,0].values)
+            
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        if self.y is not None:
+            return self.transform(self.X[idx]), self.y[idx]
+        else:
+            return self.transform(self.X[idx])
+
+
+
+
+# train_dataset = MNIST_data('./train.csv', transform= transforms.Compose(
+#                             [transforms.ToPILImage(), RandomRotation(degrees=20), RandomShift(3),
+#                              transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))]))
+# train_dataset = MNIST_data("./mnist_train.csv")#, transform= transforms.Compose(
+#                             #[transforms.ToPILImage(), RandomRotation(degrees=20), RandomShift(3),
+#                             #transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))]))
+#test_dataset = MNIST_data("./mnist_test.csv")
+
+
+train_dataset = MNIST_data(filename_train, transform= transforms.Compose(
+                            [transforms.ToPILImage(), 
+                             transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))]))
+test_dataset = MNIST_data(filename_test)
+
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, shuffle=True)
+# print('train_loader:', train_loader)
+# input('ok?')
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                           batch_size=batch_size, shuffle=False)
+
+class Net(nn.Module):    
+    def __init__(self):
+        super(Net, self).__init__()
+          
+        # self.features = nn.Sequential(
+        #     nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(32),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+        #     nn.BatchNorm2d(32),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=2, stride=2),
+        #     nn.Conv2d(32, 64, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(64),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(64, 64, kernel_size=3, padding=1),
+        #     nn.BatchNorm2d(64),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=2, stride=2)
+        # )
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p = 0.5),
+        #     nn.Linear(64 * 7 * 7, 512),
+        #     nn.BatchNorm1d(512),
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(p = 0.5),
+        #     nn.Linear(512, 512),
+        #     nn.BatchNorm1d(512),
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(p = 0.5),
+        #     nn.Linear(512, 10),
+        # )
+        
+        self.features = nn.Sequential(
+            #in_channel, out_channel
+            nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            # nn.BatchNorm2d(32),
+            # nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            # nn.BatchNorm2d(32),
+            # nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(64),
+            # nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(64),
+            # nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+          
+        self.classifier = nn.Sequential(
+            # nn.Dropout(p = 0.5),
+            #nn.Linear(64 * 7*7, 512), #7*7
+            nn.Linear(3200*COEF, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(p = 0.5),
+            nn.Linear(512, 512), #512
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(p = 0.5),
+            nn.Linear(512, n_classes), #10
+        )
+          
+        for m in self.features.children():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        
+        for m in self.classifier.children():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+                
+    def forward(self, x):
+        #print('feature...')
+        x = self.features(x)
+        #print('view...')
+        x = x.view(x.size(0), -1)
+        #print('classifier...')
+        x = self.classifier(x)
+        #print('classified...end')
+        
+        return x     
+
+#training
+model = Net()
+optimizer = optim.Adam(model.parameters(), lr=0.0005)#0.003
+criterion = nn.CrossEntropyLoss()
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+if torch.cuda.is_available():
+    model = model.cuda()
+    criterion = criterion.cuda()
+
+def train(epoch, train_loader):
+    model.train()
+    exp_lr_scheduler.step()
+
+    for batch_idx, (data, target) in enumerate(train_loader):
+    #for batch_idx, tmp in enumerate(train_loader):
+    #    data, target = tmp
+        #print('target:', target)
+        data, target = Variable(data), Variable(target)
+        
+        if torch.cuda.is_available():
+            data = data.cuda()
+            target = target.cuda()
+        
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        
+        loss.backward()
+        optimizer.step()
+        
+        if (batch_idx + 1)% 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, (batch_idx + 1) * len(data), len(train_loader.dataset),
+                100. * (batch_idx + 1) / len(train_loader), loss.data))#[0]
+
+def evaluate(data_loader):
+    model.eval()
+    loss = 0
+    correct = 0
+    
+    for data, target in data_loader:
+        #data, target = Variable(data, volatile=True), Variable(target)
+        #print('target:', target)
+        data, target = Variable(data), Variable(target)
+        if torch.cuda.is_available():
+            data = data.cuda()
+            target = target.cuda()
+        
+        output = model(data)
+        
+        loss += F.cross_entropy(output, target, size_average=False).data#[0]
+
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        
+    loss /= len(data_loader.dataset)
+    accuracy = 100.0 * float(correct) / float(len(data_loader.dataset))
+        
+    print('Average loss: {:.4f}, Accuracy: {}/{} {:.4}%'.format(
+        loss, correct, len(data_loader.dataset), accuracy))
+
+n_epochs = 2
+
+for epoch in range(n_epochs):
+    train(epoch, train_loader)
+    print('[train]')
+    evaluate(train_loader)
+    print('[test]')
+    evaluate(test_loader)
+
+#0.make function to generate abnormal status #ok
+#1.confirm raw data that >0 #ok
+#2.save ics data to csv #ok
+#3.training #ok
+#9.confirm no_grad option
+#4. data visualize function
+#4. kernel visualize
+#4. test data, add noise in every label-1 data
+        
+#normalization
+#xtrain = train.iloc[:,1:]
+#xtest = test.iloc[:,1:]
+# print(xtrain.iloc[0])
+# mean = xtrain.mean()
+# std = xtrain.std()
+# xtrain = (xtrain-mean)/std
+# print(xtrain.shape)
+# print(xtrain.iloc[0])
+
+
+# xtrain_imgs = np.reshape(np.array(xtrain), (-1, 1, 28, 28))
+# print(xtrain_imgs.shape)
+# xtest_imgs = np.reshape(np.array(xtest), (-1, 1, 28, 28))
+# print(xtest_imgs.shape)
+
+#import time
+# t0 = time.time()
+# pt = TSNE(n_components=3, n_iter=5000, init='pca', random_state=0).fit_transform(X)
+# plot_tSNE(pt, Y,"t-SNE plot(time %.2fs)" %(time() - t0))
